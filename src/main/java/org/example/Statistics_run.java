b13479c55c8ba0d43734d6a4fb6ed3215bc34375
vity <you@example.com>
2024-05-23 00:33:09 +0600

    statistic

--- src/main/java/org/example/Statistics_run.java
+++ src/main/java/org/example/Statistics_run.java
@@ -2,8 +2,7 @@ package org.example;
 
 import org.example.save_to_disk.Save_to_disk_history;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.Scanner;
 import java.util.Timer;
 
@@ -13,22 +12,40 @@ public class Statistics_run {
     public static Timer mTimer = new Timer();
 
 
+    public static void create_statistic(){
+        String text = read_log_mess();
 
-       public static void read_log_mess() throws FileNotFoundException {
+    }
 
-
-
-           Scanner scanner = new Scanner(new File(Save_to_disk_history.log_log));
-            while (scanner.hasNextLine()) {
-              // System.out.println(scanner.nextLine());
-                scanner.nextLine();
-            }
-            scanner.close();
+    public static String read_log_mess()  {
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(Save_to_disk_history.log_log));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
         }
+        try {
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
 
-
-
-
+            while (line != null) {
+                sb.append(line);
+                sb.append(System.lineSeparator());
+                line = br.readLine();
+            }
+            String everything = sb.toString();
+            System.out.println(everything);
+            return everything;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                br.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 
 
 }
